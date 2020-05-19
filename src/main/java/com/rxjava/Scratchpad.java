4079a149fd305cd02708007761174a132c436c35
Archit <archtkejariwal@gmail.com>
2020-05-19 21:45:55 +0530

    ServiceAggregation, Database examples

--- src/main/java/com/rxjava/Scratchpad.java
+++ src/main/java/com/rxjava/Scratchpad.java
@@ -1,18 +1,24 @@
 package com.rxjava;
 
+import com.rxjava.utility.ThreadHelper;
 import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.internal.functions.Functions;
+
 import java.util.concurrent.TimeUnit;
 
 public class Scratchpad {
 
-    public static void main(String[] args){
-        Observable<Long> newsRefreshes = Observable.interval(100, TimeUnit.SECONDS);
-        Observable<Long> weatherRefreshes = Observable.interval(50, TimeUnit.SECONDS);
-        Observable.combineLatest(newsRefreshes, weatherRefreshes,
-                (newsRefreshTimes, weatherRefreshTimes) ->
-                        "Refreshed news " + newsRefreshTimes + " times and weather " + weatherRefreshTimes)
-                .subscribe(x -> {
-                    System.out.println(x);
-                });
+    public static void main(String[] args) {
+        Observable<Observable<String>> timeIntervals =
+                Observable.interval(1, TimeUnit.SECONDS)
+                        .map(ticks -> Observable.interval(100, TimeUnit.MILLISECONDS)
+                                .map(innerInterval -> "outer: " + ticks + " - inner: " + innerInterval));
+        Disposable flatmap = timeIntervals.flatMap(Functions.identity()).subscribe(System.out::println);
+        ThreadHelper.sleep(3, TimeUnit.SECONDS);
+        flatmap.dispose();
+        System.out.println("\n\n---------------------------------");
+        Observable.switchOnNext(timeIntervals).subscribe(System.out::println);
+        ThreadHelper.sleep(3, TimeUnit.SECONDS);
     }
 }
